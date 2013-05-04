878182d3b1c70d5c6faa07fbc604d100cb421064
Nimit Suwannagate <ethaizone@hotmail.com>
2013-05-04 10:29:45 +0700

    Call theme() method when obj construct
    Call theme() method when obj construct
    
    This will help when we call of() method by static.
    
    Theme::of('admin.home.index')->render();

--- src/Teepluss/Theme/Theme.php
+++ src/Teepluss/Theme/Theme.php
@@ -68,6 +68,9 @@ class Theme {
 		// Default theme and layout.
 		$this->theme  = $this->config->get('theme::themeDefault');
 		$this->layout = $this->config->get('theme::layoutDefault');
+		
+		//Make of() method can call static
+		$this->theme();
 	}
 
 	/**
