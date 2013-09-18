8694c13db20da0658246a142b3bdb44b3d136f5a
Tee <admin@jquerytips.com>
2013-09-18 15:26:43 +0700

    add test case

---
+++ tests/ThemeTest.php
@@ -0,0 +1,15 @@
+<?php
+
+class ThemeTest extends PHPUnit_Framework_TestCase {
+
+    public function testSomethingIsTrue()
+    {
+        $this->assertTrue(true);
+    }
+
+    public function testSomethingElseIsTrue()
+    {
+        $this->assertTrue(false);
+    }
+
+}
