cbdfc3ad9a17e61422bd7dfa93dddab8a26ca80b
Tee <admin@jquerytips.com>
2013-10-01 14:20:43 +0700

    remove comment asset

--- src/Teepluss/Theme/AssetQueue.php
+++ src/Teepluss/Theme/AssetQueue.php
@@ -46,8 +46,8 @@ class AssetQueue extends AssetContainer {
         $hashed = $this->hashed($group, $anames);
 
         // Remove comments.
-        $buffer = preg_replace("!/*[^*]**+([^/][^*]**+)*/!", '', $buffer);
-        $buffer = preg_replace( "/(?<!:)\/\/(.*)\n/", '', $buffer );
+        $buffer = preg_replace('!/\*.*?\*/!s', '', $buffer);
+        $buffer = preg_replace('/^\s*\/\/(.+?)$/m', "\n", $buffer);
 
         // Remove tabs, spaces, newlines, etc.
         $buffer = str_replace(array("\r\n","\r","\n","\t",'  ','    ','     '), '', $buffer);
