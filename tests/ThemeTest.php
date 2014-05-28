4ab3f8b57b63df8177eb71373d810de228ae6e38
Teepluss <admin@jquerytips.com>
2014-05-29 00:23:41 +0700

    test case

---
+++ tests/ThemeTest.php
@@ -0,0 +1,12 @@
+<?php
+
+use PHPUnit_Framework_TestCase;
+
+class ThemeTest extends PHPUnit_Framework_TestCase {
+
+    public function testExample()
+    {
+        return $this->assertEquals('name', 's');
+    }
+
+}
