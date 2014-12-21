165ba0c1cbc486042ead73a9ae0681d25027acb9
AgelxNash <agel_nash@xaker.ru>
2014-12-22 03:33:18 +0500

    JSMin Exceptions

--- src/Teepluss/Theme/Exceptions.php
+++ src/Teepluss/Theme/Exceptions.php
@@ -6,3 +6,7 @@ class UnknownLayoutFileException extends \UnexpectedValueException {}
 class UnknownWidgetFileException extends \UnexpectedValueException {}
 class UnknownWidgetClassException extends \UnexpectedValueException {}
 class UnknownPartialFileException extends \UnexpectedValueException {}
+
+class JSMin_UnterminatedRegExpException extends \Exception {}
+class JSMin_UnterminatedStringException extends  \Exception {}
+class JSMin_UnterminatedCommentException extends  \Exception {}
